04d5992312eb2f3e0a0f95957ad4c9b5925eaa9a
blazexYT <22298@burnside.school.nz>
2025-05-16 16:34:34 +1200

    loginpage

--- routes.py
+++ routes.py
@@ -1,17 +1,20 @@
 from flask import Flask, render_template
 import colorama
-from colorama import Fore, Back, Style 
+from colorama import Fore, Back, Style
+
 colorama.init(autoreset=True)
 import sqlite3
 
 app = Flask(__name__)
-@app.route('/')
+
+
+@app.route("/")
 def home():
     header = "Home"
     return render_template("home.html", header=header)
 
 
-@app.route('/test')
+@app.route("/test")
 def test():
     header = "Test me"
     return render_template("test.html", header=header)
@@ -22,36 +25,41 @@ def pizza():
     header = "pizza"
     conn = sqlite3.connect("pizza.db")
     cursor = conn.cursor()
-    cursor.execute('SELECT * FROM Pizza')
+    cursor.execute("SELECT * FROM Pizza")
     pizzas = cursor.fetchall()
     conn.close()
     return render_template("pizzas.html", header=header, pizzas=pizzas)
 
 
-@app.route('/about')
+@app.route("/about")
 def about():
     header = "About"
     return render_template("about.html", header=header)
 
 
-@app.route('/subject')
+@app.route("/subject")
 def subject_selection():
     header = "Subject Selection"
     return render_template("subject_selection.html", header=header)
 
 
-
-@app.route('/help')
+@app.route("/help")
 def help():
     header = "Help"
     return render_template("help.html", header=header)
 
-@app.route('/technology')
+
+@app.route("/technology")
 def technology():
     header = "tech"
-
     return render_template("technology.html", header=header)
 
 
-if __name__ == '__main__':
+@app.route("/login")
+def login():
+    header = "login"
+    return render_template("login.html", header=header)
+
+
+if __name__ == "__main__":
     app.run(debug=True)

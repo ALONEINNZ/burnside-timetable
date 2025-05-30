7be6713c782f49c68c19a2bca5600661427f7d04
blazexYT <22298@burnside.school.nz>
2025-05-30 16:23:59 +1200

    routes

--- routes.py
+++ routes.py
@@ -1,11 +1,18 @@
-from flask import Flask, render_template
+from inspect import ismethod
+import os
+from pathlib import Path
+from flask import Flask, flash, redirect, render_template, request, session, url_for
 import colorama
 from colorama import Fore, Back, Style
+from flask.cli import load_dotenv
 
 colorama.init(autoreset=True)
 import sqlite3
 
 app = Flask(__name__)
+dotenv_path = Path(".env")
+load_dotenv(dotenv_path)
+app.secret_key = os.getenv("KEY")
 
 
 @app.route("/")
@@ -55,10 +62,27 @@ def technology():
     return render_template("technology.html", header=header)
 
 
-@app.route("/login")
+@app.route("/login", methods=["GET", "POST"])
 def login():
     header = "login"
-    return render_template("login.html", header=header)
+    error = None
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+        conn = sqlite3.connect("main.db")
+        cursor = conn.cursor()
+        cursor.execute("SELECT * FROM users")
+        users = cursor.fetchall()
+        conn.close()
+        for user in users:
+            if username == user[1]:
+                if password == user[2]:
+                    session["username"] = username
+                    session["pfp"] = user[3]
+                    flash("You were successfully logged in")
+                    return redirect(url_for("home"))
+        error = "invald username/password"
+    return render_template("login.html", header=header, error=error)
 
 
 if __name__ == "__main__":
